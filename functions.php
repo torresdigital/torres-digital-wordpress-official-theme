2bf4e34b19d026e86c850f50faedd9bee46ccd0b
Anderson Torres <atorresnh@gmail.com>
2019-07-08 08:00:15 -0300

    Firest Commit of archives
    Primeiro Commit dos arquivos do Tema

---
+++ functions.php
@@ -0,0 +1,296 @@
+<?php /* Theme Name: 🌟Torres Digital®
+
+Theme URI: https://www.facebook.com/torresdigital/
+
+    Author: Torres Digital® | Sites → Lojas Virtuais e e-Commerce
+    Author URI: https://www.facebook.com/torresdigital/
+
+    Description: Somos uma Agência Gaúcha que trabalha com Desenvolvimento Web voltado para todos os Nichos do Mercado tais como os de insumos, commodities, pequenos, médios e grandes Lojistas que desejam alcançar mais Clientes através do e-Commerce: Sites, Aplicativos, Lojas Virtuais, Marketplaces, WordPress e Woocommerce, integrados com os Principais Cartões e Soluções de Pagamentos do Brasil e do Mundo; tais como Cielo, CyberSource, PagSeguro, Stripe, Vindi, MasterCard, Visa, American Express, outros.
+
+    Torres Digital também conta com toda uma Equipe e um Know-how forte, amplo, e moderno e conceitual para a organização e criação de todo um Portifólio para Publicidade e Propaganda.
+
+    www.torresdigital.tk * Menos é mais.
+
+    Text Domain: torresdigital
+    Template: torresdigital
+    Version: 2.0 *//*
+
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Tags: child theme, woocommerce
+Text Domain: torresdigital
+This theme, like WordPress, is licensed under the GPL. Use it to make something cool, have fun, and share what you’ve learned with others.  */
+
+/*All Begin*/
+
+/****
+    * Chamando o Tema Pai * Quema Labs
+    */
+
+function child_shophistic_theme_enqueue_styles() {
+
+    //First we load Bootstrap from parent, then parent styles and then child styles
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array( 'bootstrap' ) );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
+
+}
+add_action( 'wp_enqueue_scripts', 'child_shophistic_theme_enqueue_styles' );
+
+
+/****
+    * Chamando o Logo
+    ************************************/
+function themename_custom_logo_setup() {
+    $defaults = array(
+        'height' => 100,
+        'width' => 400,
+        'flex-height' => true,
+        'flex-width' => true,
+        'header-text' => array('site-title', 'site-description'),
+    );
+    add_theme_support('custom-logo', $defaults);
+}
+add_action('after_setup_theme', 'themename_custom_logo_setup');
+
+function my_excerpt_length($length) {
+    return 110;
+}
+
+add_filter('excerpt_length', 'my_excerpt_length');
+
+function mytheme_add_woocommerce_support() {
+	add_theme_support( 'woocommerce', array(
+		'thumbnail_image_width' => 600,
+		'single_image_width'    => 600,
+
+        'product_grid'          => array(
+            'default_rows'    => 3,
+            'min_rows'        => 2,
+            'max_rows'        => 8,
+            'default_columns' => 4,
+            'min_columns'     => 2,
+            'max_columns'     => 5,
+        ),
+	) );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
+/**
+ * Generate breadcrumbs
+ * @author CodexWorld
+ * @authorURL www.codexworld.com
+ */
+function get_breadcrumb() {
+    echo ':: <a href="'.home_url().'" rel="nofollow">index</a> ::';
+    if (is_category() || is_single()) {
+        echo "&nbsp;&nbsp;&#187;&nbsp;&nbsp;";
+        the_category(' &bull; ');
+            if (is_single()) {
+                echo " &nbsp;&nbsp;&#187;&nbsp;&nbsp; ";
+                the_title();
+            }
+    } elseif (is_page()) {
+        echo "&nbsp;&nbsp;&#187;&nbsp;&nbsp;";
+        echo the_title();
+    } elseif (is_search()) {
+        echo "&nbsp;&nbsp;&#187;&nbsp;&nbsp;Search Results for... ";
+        echo '"<em>';
+        echo the_search_query();
+        echo '</em>"';
+    }
+}
+/****
+    * Chamando o Tema Pai * v2
+    *
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+
+/****
+    * Chamando o Tema Pai * v1
+    *
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
+
+/* registrando scripts *
+add_action("wp_enqueue_scripts", "scripts");
+function myscripts() { 
+    wp_register_script('tdwct', 
+                        get_template_directory_uri() .'/js/scripts.js',   //
+                        array ('jquery', 'jquery-ui'),                  //depends on these, however, they are registered by core already, so no need to enqueue them.
+                        false, false);
+    wp_enqueue_script('tdwct');
+
+}
+
+/****
+    * Registrando Scripts v.2 *
+    */
+
+/* Registrando Scripts v.2 *
+function my_enqueue_scripts()
+{
+    wp_register_script( 'first', get_template_directory_uri() . 'js/first.js' );
+ 
+    wp_enqueue_script( 'second', get_template_directory_uri() . 'js/second.js', array( 'first' ) );
+}
+add_action( 'wp_enqueue_scripts', 'my_enqueue_scripts' ); 
+
+*/
+
+
+/****
+    * Registrando Scripts v.3 *
+    */
+
+/* Registrando Scripts v.3 *
+
+function myscripts() {
+    //get some external script that is needed for this script
+    wp_enqueue_script('jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js'); 
+    $script = get_template_directory_uri() . '/library/myscript.js';
+    wp_register_script('myfirstscript', 
+                        $script, 
+                        array ('jquery', 'jquery-ui'), 
+                        false, false);
+    //always enqueue the script after registering or nothing will happen
+    wp_enqueue_script('fullpage-slimscroll');
+
+}
+add_action("wp_enqueue_scripts", "myscripts");
+
+/**
+    * Change the login page icon and URL to our site instead of WordPress.org
+    */
+    add_filter( 'login_headerurl', 'xs_login_headerurl' );
+    function xs_login_headerurl( $url ) {
+    return esc_url(  'https://www.torresdigital.tk.'  );
+    }
+    add_filter( 'login_headertitle', 'xs_login_headertitle' );
+    function xs_login_headertitle( $title ) {
+    return get_bloginfo ( 'name' ) . ' – ' . get_bloginfo ( 'description' );
+    }
+
+
+/* Outros */
+
+ /** Font Awesome, by Torres Digital */
+  function theme_enqueue_styles() {
+    $parent_style = 'parent-style';
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'my-child-fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style )
+    );
+}
+    /**
+     * Order product collections by stock status, instock products first.
+     * https://stackoverflow.com/questions/25113581/show-out-of-stock-products-at-the-end-in-woocommerce
+     */
+    class iWC_Orderby_Stock_Status
+    {
+
+        public function __construct()
+        {
+            // Check if WooCommerce is active
+            if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+                add_filter('posts_clauses', array($this, 'order_by_stock_status'), 2000);
+            }
+        }
+
+        public function order_by_stock_status($posts_clauses)
+        {
+            global $wpdb;
+            // only change query on WooCommerce loops
+            if (is_woocommerce() && (is_shop() || is_product_category() || is_product_tag())) {
+                $posts_clauses['join'] .= " INNER JOIN $wpdb->postmeta istockstatus ON ($wpdb->posts.ID = istockstatus.post_id) ";
+                $posts_clauses['orderby'] = " istockstatus.meta_value ASC, " . $posts_clauses['orderby'];
+                $posts_clauses['where'] = " AND istockstatus.meta_key = '_stock_status' AND istockstatus.meta_value <> '' " . $posts_clauses['where'];
+            }
+            return $posts_clauses;
+        }
+    }
+
+    new iWC_Orderby_Stock_Status;
+
+
+    /*
+
+    function child_shophistic_theme_enqueue_styles() {
+
+	//First we load Bootstrap from parent, then parent styles and then child styles
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array( 'bootstrap' ) );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
+
+    }
+    add_action( 'wp_enqueue_scripts', 'child_shophistic_theme_enqueue_styles' );
+
+    register_sidebar( array(
+    'name' => 'Footer Sidebar 1',
+    'id' => 'footer-sidebar-1',
+    'description' => 'Appears in the footer area',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title' => '</h3>',
+    ) );
+    register_sidebar( array(
+    'name' => 'Footer Sidebar 2',
+    'id' => 'footer-sidebar-2',
+    'description' => 'Appears in the footer area',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title' => '</h3>',
+    ) );
+    register_sidebar( array(
+    'name' => 'Footer Sidebar 3',
+    'id' => 'footer-sidebar-3',
+    'description' => 'Appears in the footer area',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title' => '</h3>',
+    ) );
+    register_sidebar( array(
+    'name' => 'Footer Sidebar 4',
+    'id' => 'footer-sidebar-4',
+    'description' => 'Appears in the footer area',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title' => '</h3>',
+    ) );
+
+
+    function my_login_logo_one() {  ?> 
+        <style type="text/css"> 
+        body.login div#login h1 a {
+            background-image: url(/wp-content/uploads/2018/10/logo-sites-torresdigital.png);
+            background-size: 70%;
+            width: 100%;
+            height: 237px;
+            text-align: center;
+            position: relative;
+            margin: 0 auto;
+            left: 17px;
+                                    }
+        </style> 
+    <?php  } add_action( 'login_enqueue_scripts', 'my_login_logo_one' );
---
+++ header.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140394692-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-140394692-1');
+    </script>
+
+
+    <!--[if lt IE 9]>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5shiv.js"></script>
+    <![endif]-->
+
+	<meta name="description" content="<?php bloginfo('description'); ?>">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <!-- WP_Head -->
+    	<?php wp_head(); ?>
+   <!-- /WP_Head -->
+</head>
+
+<div id="sudo-su" class="torres-digital">
+<body <?php body_class(); ?>>
+
+	<div id="wrap">
+            <header id="header">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="ql_logo_nav col-md-9 col-xs-12">
+
+                            <div class="login_cart_wrap col-md-3 col-xs-12">
+                                <div class="ql_cart_wrap">
+                                    <button href="<?php echo esc_url( WC()->cart->get_cart_url() ); ?>" class="ql_cart-btn">
+                                        <?php echo wp_kses_post( WC()->cart->get_cart_total() ); ?>
+                                        <span class="count">(<?php echo esc_html( WC()->cart->cart_contents_count );?>)</span>
+                                        <i class="ql-bag"></i><i class="ql-chevron-down"></i>
+                                    </button>
+
+                                    <div id="ql_woo_cart">
+                                        <?php global $woocommerce; ?>
+                                        <?php the_widget('WC_Widget_Cart');  ?>
+                                    </div><!-- /ql_woo_cart --> 
+                                </div>
+                                <div class="login_btn_wrap">
+                                    <?php if ( is_user_logged_in() ) { ?>
+                                        <a class="ql_login-btn" href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ); ?>" title="<?php esc_attr_e( 'My Account', 'shophistic-lite' ); ?>"><?php esc_html_e( 'My Account', 'shophistic-lite' ); ?></a>
+                                     <?php } 
+                                     else { ?>
+                                        <a class="ql_login-btn" href="<?php echo esc_url( get_permalink( get_option('woocommerce_myaccount_page_id') ) ); ?>" title="<?php esc_attr_e( 'Login', 'shophistic-lite' ); ?>"><?php esc_html_e( 'Login', 'shophistic-lite' ); ?></a>
+                                     <?php } ?>
+                                </div>
+                                <div class="clearfix"></div>
+                            </div><!-- col-md-4 -->
+
+                        <div class="logo_container"> 
+                            <!--  <a href="< ?php echo home_url(); ?>/" class="ql_logo google-font">
+                                < ?php bloginfo('name'); ?> </a> -->
+                                <button id="ql_nav_btn" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#ql-navigation" aria-expanded="false">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                </button>
+                            </div><!-- /logo_container -->
+
+
+                        <div id="torres-digita-logo" class="torres-digita-logo">
+                            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+                                <img src="/wp-content/themes/torresdigital/img/logo22019.jpg" alt="Logo" width="138" height="156" />
+                            </a>
+                        </div>
+
+                            <div class="collapse navbar-collapse" id="ql-navigation">
+                                <nav id="jqueryslidemenu" class="jqueryslidemenu navbar " role="navigation">
+                                    <?php
+                                    if ( has_nav_menu( 'menu-1' ) ){ 
+                                            wp_nav_menu( array(
+                                            'theme_location'  => 'menu-1',
+                                            'depth'             => 3,
+                                            'menu_class'        => 'nav navbar-nav',
+                                            'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                                            'walker'            => new wp_bootstrap_navwalker()
+                                        )); 
+                                    }else{
+                                        echo "<ul id='nav' class='nav navbar-nav'>";
+                                        wp_list_pages( array(
+                                            'title_li'     => ''
+                                            )
+                                        );
+                                        echo "</ul>";
+                                    }; 
+                                    ?>
+                                </nav>
+                            </div>
+                            <div class="clearfix"></div>
+
+                        </div><!-- col-md-8 -->
+
+                        <?php
+                       if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+                        ?>
+                        <?php } //if WooCommerce active ?>
+
+                    </div><!-- row-->
+                </div><!-- /container -->
+
+                <div id="torres-digital-background-head" class="torres-digital-background-head"></div>
+
+
+
+    <div class="clearfix"></div>
