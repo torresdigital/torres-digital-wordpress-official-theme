5954142622746488fd350425ca20dd2c3550fccf
Anderson Torres <atorresnh@gmail.com>
2019-08-03 04:59:21 -0300

    Update header.php

--- header.php
+++ header.php
@@ -24,11 +24,21 @@
         });
     </script>
 
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5F54FW');</script>
+<!-- End Google Tag Manager -->
+
 
     <!--[if lt IE 9]>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5shiv.js"></script>
     <![endif]-->
+
+    <meta name="msvalidate.01" content="F9C90E00BD4564D0E17C680D6456AA3E" />
 
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 
@@ -40,6 +50,11 @@
 </head>
 
 <body <?php body_class(); ?> >
+
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5F54FW"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
     <div id="sudo-su" class="torres-digital sudo-su">
 
